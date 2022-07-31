8ef17964acf05aca05afb9c215c44fc43e8422fd
Jason Vance <jbvance@gmail.com>
2022-07-31 08:56:31 -0500

    refactor to move routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import { createContext, useState } from 'react';
+import React, { createContext, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
 import MultiStepForm from './components/MultiStepForm';
+import AuthForm from './components/AuthForm';
+import store from './store';
+
 export const FormContext = createContext({
   activeStepIndex: 0,
   setActiveStepIndex: (prevState: number) => {},
-  steps: ['client-info', 'children', 'dpoa']
+  steps: ['client-info', 'children', 'dpoa'],
 });
 
 function App() {
   const [activeStepIndex, setActiveStepIndex] = useState(0);
   const steps = ['client-info', 'children', 'dpoa'];
   return (
-    <FormContext.Provider
-      value={{ activeStepIndex, setActiveStepIndex, steps }}
-    >
-      <div className="container">
-        <MultiStepForm />
-      </div>
-    </FormContext.Provider>
+    <React.StrictMode>
+      <FormContext.Provider
+        value={{ activeStepIndex, setActiveStepIndex, steps }}
+      >
+        <BrowserRouter>
+          <Provider store={store}>
+            <Routes>
+              <Route path="/" element={<MultiStepForm />} />
+              <Route path="/auth" element={<AuthForm />} />
+            </Routes>
+          </Provider>
+        </BrowserRouter>
+      </FormContext.Provider>
+    </React.StrictMode>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -11,18 +11,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/auth" element={<AuthForm />} />
-        </Routes>
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
